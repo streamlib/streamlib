148741f18b3781cbca00caf43780a7e86cf62e39
Yuval Adam <_@yuv.al>
2019-09-22 01:00:17 +0300

    Initial clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Streamlib");
+    let matches = App::new("Streamlib")
+        .version("0.1")
+        .author("Yuval Adam")
+        .about("A video stream meta-player and specification")
+        .arg(Arg::with_name("player")
+            .short("p")
+            .long("player")
+            .help("Media player to run, defaults to `mpv`")
+            .takes_value(true))
+        .get_matches();
+    println!("{}", matches.value_of("player").unwrap_or("mpv"));
 }
