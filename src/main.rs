7ffbcee9edf19235257ffe8a9f05a2f4f3e5d4cf
Yuval Adam <_@yuv.al>
2019-09-22 21:58:19 +0300

    Test WIP

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,17 @@ fn main() {
         .version("0.1")
         .author("Yuval Adam")
         .about("A video stream meta-player and specification")
+        .arg(Arg::with_name("library")
+            .help("TOML file library")
+            .required(true)
+            .index(1))
         .arg(Arg::with_name("player")
             .short("p")
             .long("player")
             .help("Media player to run, defaults to `mpv`")
             .takes_value(true))
         .get_matches();
+
     println!("{}", matches.value_of("player").unwrap_or("mpv"));
+    println!("{}", matches.value_of("library").unwrap());
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,28 @@
 extern crate toml;
+extern crate serde;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Entry {
+    name: Option<String>,
+    description: Option<String>,
+    url: String,
+    tags: Vec<String>
+}
+
+#[derive(Deserialize, Debug)]
+struct Library {
+    title: Option<String>,
+    entries: Vec<Entry>
+}
 
 #[cfg(test)]
 mod tests {
     use toml::Value;
+    use super::Library;
 
-    #[test]
-    fn test_parse() {
-        let val = r#"
+    const TEST_LIB: &'static str = r#"
         title = "SomaFm Radio Stations"
 
         [groovesalad]
@@ -14,8 +30,20 @@ mod tests {
         description = "A nicely chilled plate of ambient/downtempo beats and grooves"
         url = "http://somafm.com/groovesalad.pls"
         tags = ["somafm", "radio", "ambient", "groove"]
-        "#.parse::<Value>().unwrap();
+        "#;
 
+    #[test]
+    fn test_parse() {
+        let val = TEST_LIB.parse::<Value>().unwrap();
         assert_eq!(val["title"].as_str(), Some("SomaFm Radio Stations"));
+        let m = val.as_table().unwrap();
+        for v in m.iter() {
+            println!("{}", v.0);
+        }
+    }
+
+    #[test]
+    fn test_struct_parse() {
+        // let val: Library = toml::from_str(TEST_LIB).unwrap();
     }
 }
