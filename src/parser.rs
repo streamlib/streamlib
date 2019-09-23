3342b65624d70ad5fcbea4755daafc5a4b8ca136
Yuval Adam <_@yuv.al>
2019-09-23 10:43:29 +0300

    Fix Library type parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 extern crate toml;
 extern crate serde;
 
+use std::collections::BTreeMap;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -8,42 +9,30 @@ struct Entry {
     name: Option<String>,
     description: Option<String>,
     url: String,
-    tags: Vec<String>
+    tags: Option<Vec<String>>
 }
 
-#[derive(Deserialize, Debug)]
-struct Library {
-    title: Option<String>,
-    entries: Vec<Entry>
-}
+type Library = BTreeMap<String, Entry>;
 
 #[cfg(test)]
 mod tests {
-    use toml::Value;
     use super::Library;
 
     const TEST_LIB: &'static str = r#"
-        title = "SomaFm Radio Stations"
-
         [groovesalad]
         name = "Groove Salad"
         description = "A nicely chilled plate of ambient/downtempo beats and grooves"
         url = "http://somafm.com/groovesalad.pls"
         tags = ["somafm", "radio", "ambient", "groove"]
+
+        [secretagent]
+        url = "http://somafm.com/secretagent.pls"
         "#;
 
     #[test]
-    fn test_parse() {
-        let val = TEST_LIB.parse::<Value>().unwrap();
-        assert_eq!(val["title"].as_str(), Some("SomaFm Radio Stations"));
-        let m = val.as_table().unwrap();
-        for v in m.iter() {
-            println!("{}", v.0);
-        }
-    }
-
-    #[test]
     fn test_struct_parse() {
-        // let val: Library = toml::from_str(TEST_LIB).unwrap();
+        let lib: Library = toml::from_str(TEST_LIB).unwrap();
+        assert_eq!(lib.get("groovesalad").unwrap().name, Some(String::from("Groove Salad")));
+        assert_eq!(lib.get("secretagent").unwrap().url, String::from("http://somafm.com/secretagent.pls"));
     }
 }
