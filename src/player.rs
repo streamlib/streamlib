52d5ae01c11dcb973570f25a894a33cce083ceeb
Yuval Adam <_@yuv.al>
2019-10-09 21:17:12 +0300

    Implement append_query_args()

--- src/player.rs
+++ src/player.rs
@@ -3,12 +3,14 @@ extern crate reqwest;
 extern crate serde_json;
 
 use duct::cmd;
-use super::library::Entry;
+use std::collections::HashMap;
+use super::library::{Entry, Query};
 
 pub struct Player {
     player: String,
     url: String,
     headers: Vec<String>,
+    queries: Vec<Query>,
     debug: bool
 }
 
@@ -18,6 +20,7 @@ impl Player {
             player: String::from(player),
             url: entry.url,
             headers: entry.http_headers.unwrap_or(vec![]),
+            queries: entry.query.unwrap_or(vec![]),
             debug: false
         }
     }
@@ -35,8 +38,30 @@ impl Player {
             args.push(self.headers.join("','"));
         }
 
+        if self.queries.len() > 0 {
+
+        }
+
         args.push(self.url.clone());
         args
+    }
+
+    pub fn append_query_args(&mut self, args: HashMap<&str, &str>) {
+        let mut res = String::new();
+        for (key, value) in args {
+            res.push_str(key);
+            res.push_str("=");
+            res.push_str(value);
+            res.push_str("&");
+        }
+        res.pop(); // remove the last ampersand
+        if self.url.contains("?") {
+            self.url.push_str("&");
+        }
+        else {
+            self.url.push_str("?");
+        }
+        self.url.push_str(&res);
     }
 
     pub fn play(&self) {
@@ -58,6 +83,7 @@ impl Player {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use super::Player;
     use super::super::utils::json_query;
 
@@ -67,6 +93,7 @@ mod tests {
             player: String::from("mpv"),
             url: String::from("http://example.com/"),
             headers: vec![String::from("A: b"), String::from("C: d")],
+            queries: vec![],
             debug: false
         };
         assert_eq!(p.build_args(), ["--http-header-fields", "A: b','C: d", "http://example.com/"]);
@@ -78,6 +105,7 @@ mod tests {
             player: String::from("cvlc"),
             url: String::from("http://example.com/"),
             headers: vec![String::from("A: b"), String::from("C: d")],
+            queries: vec![],
             debug: false
         };
         assert_eq!(p.build_args().len(), 1); // just the url
@@ -89,6 +117,7 @@ mod tests {
             player: String::from("mpv"),
             url: String::from("http://example.com/"),
             headers: vec![],
+            queries: vec![],
             debug: true
         };
         assert_eq!(p.build_args(), ["-v", "http://example.com/"]);
@@ -100,6 +129,7 @@ mod tests {
             player: String::from("nonexistentplayer"),
             url: String::from("http://example.com/"),
             headers: vec![],
+            queries: vec![],
             debug: false
         };
         // will throw an error if not caught
@@ -111,5 +141,38 @@ mod tests {
         let resp: serde_json::Value = reqwest::get("https://mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/hls/live/512033/CH2LIVE_HIGH/index.m3u8&rv=AKAMAI").unwrap().json().unwrap();
         let jq = "tickets.0.ticket";
         println!("{}", json_query(&resp, jq));
+    }
+
+    #[test]
+    fn test_append_args() {
+        let mut p = Player {
+            player: String::from("nonexistentplayer"),
+            url: String::from("http://example.com/feed.m3u8"),
+            headers: vec![],
+            queries: vec![],
+            debug: false
+        };
+        let mut args = HashMap::new();
+        args.insert("abc", "def");
+        p.append_query_args(args);
+
+        assert_eq!(p.url, "http://example.com/feed.m3u8?abc=def")
+    }
+
+    #[test]
+    fn test_append_args_with_existing() {
+        let mut p = Player {
+            player: String::from("nonexistentplayer"),
+            url: String::from("http://example.com/feed.m3u8?old=args"),
+            headers: vec![],
+            queries: vec![],
+            debug: false
+        };
+        let mut args = HashMap::new();
+        args.insert("abc", "def");
+        args.insert("ghi", "jkl");
+        p.append_query_args(args);
+
+        assert_eq!(p.url, "http://example.com/feed.m3u8?old=args&abc=def&ghi=jkl")
     }
 }
